7538fa885c4cd7b2b10c5297a8f1da480e0148d7
chenxiaoyu233 <x312035@gmail.com>
2018-09-23 10:57:05 +0800

    第二轮训练完毕 70%

--- Train.cpp
+++ Train.cpp
@@ -28,43 +28,56 @@ DenseLayer *Output;
 
 Estimator_Softmax *estimator;
 
-const int isTrain = 1; // TrainFlag
+const bool isTrain = false; // TrainFlag
 
 void buildNetwork() {
 	// todo
 	Input = new DenseLayer(windowWidth, frameWidth);
 	C1 = new ConvLayer(16, 60, 30, 21, 11, 1, 1, 0, 0);
-	//Dp1 = new DropoutLayer(16, 11, 33, 0.8, isTrain);
+	Dp1 = new DropoutLayer(16, 60, 30, 0.5, isTrain);
 	S1 = new ConvLayer(16, 15, 15, 4, 2, 4, 2, 0, 0);
 	C2 = new ConvLayer(16, 10, 10, 6, 6, 1, 1, 0, 0);
-	//Dp2 = new DropoutLayer(16, 2, 8, 0.8, isTrain);
+	Dp2 = new DropoutLayer(16, 10, 10, 0.5, isTrain);
 	D1 = new DenseLayer(1, 32);
 	D2 = new DenseLayer(1, 128);
-	//Dp3 = new DropoutLayer(1, 1, 128, 0.8, isTrain);
+	Dp3 = new DropoutLayer(1, 1, 128, 0.5, isTrain);
 	Output = new DenseLayer(1, 6); // for 10 case
 
 	estimator = new Estimator_Softmax(Output);
 
+#ifdef ENABLE_CUDA
 	Input -> SetActionFunc(kernel_Linear, kernel_LinearDel);
 	C1 -> SetActionFunc(kernel_tanh, kernel_tanhDel);
-	//Dp1 -> SetActionFunc(kernel_Linear, kernel_LinearDel);
+	Dp1 -> SetActionFunc(kernel_Linear, kernel_LinearDel);
 	S1 -> SetActionFunc(kernel_Linear, kernel_LinearDel);
 	C2 -> SetActionFunc(kernel_tanh, kernel_tanhDel);
-	//Dp2 -> SetActionFunc(kernel_Linear, kernel_LinearDel);
+	Dp2 -> SetActionFunc(kernel_Linear, kernel_LinearDel);
 	D1 -> SetActionFunc(kernel_tanh, kernel_tanhDel);
 	D2 -> SetActionFunc(kernel_tanh, kernel_tanhDel);
-	//Dp3 -> SetActionFunc(kernel_Linear, kernel_LinearDel);
+	Dp3 -> SetActionFunc(kernel_Linear, kernel_LinearDel);
 	Output -> SetActionFunc(kernel_Linear, kernel_LinearDel);
+#else
+	Input -> SetActionFunc(&ActiveFunction::Linear, &ActiveFunction::LinearDel);
+	C1 -> SetActionFunc(&ActiveFunction::tanh, &ActiveFunction::tanhDel);
+	Dp1 -> SetActionFunc(&ActiveFunction::Linear, &ActiveFunction::LinearDel);
+	S1 -> SetActionFunc(&ActiveFunction::Linear, &ActiveFunction::LinearDel);
+	C2 -> SetActionFunc(&ActiveFunction::tanh, &ActiveFunction::tanhDel);
+	Dp2 -> SetActionFunc(&ActiveFunction::Linear, &ActiveFunction::LinearDel);
+	D1 -> SetActionFunc(&ActiveFunction::tanh, &ActiveFunction::tanhDel);
+	D2 -> SetActionFunc(&ActiveFunction::tanh, &ActiveFunction::tanhDel);
+	Dp3 -> SetActionFunc(&ActiveFunction::Linear, &ActiveFunction::LinearDel);
+	Output -> SetActionFunc(&ActiveFunction::Linear, &ActiveFunction::LinearDel);
+#endif
 
 	C1 -> InputLayer(Input);
-	//Dp1 -> InputLayer(C1);
-	S1 -> InputLayer(C1);
+	Dp1 -> InputLayer(C1);
+	S1 -> InputLayer(Dp1);
 	C2 -> InputLayer(S1);
-	//Dp2 -> InputLayer(C2);
-	D1 -> InputLayer(C2);
+	Dp2 -> InputLayer(C2);
+	D1 -> InputLayer(Dp2);
 	D2 -> InputLayer(D1);
-	//Dp3 -> InputLayer(D2);
-	Output -> InputLayer(D2);
+	Dp3 -> InputLayer(D2);
+	Output -> InputLayer(Dp3);
 }
 
 vector <Matrix<double>*> trainData;
@@ -83,15 +96,8 @@ void readSingleData(string dataPathStr, int idx, vector<Matrix<double>*> &data, 
 	if(frameNum <= 0) return;
 	Matrix<double> tmp(frameNum, frameWidth);
 	for(int i = 1; i <= frameNum; i++) {
-		double len = 0;
 		for(int j = 1; j <= frameWidth; j++) {
 			fin >> tmp(i, j);
-			len += tmp(i, j) * tmp(i, j);
-		}
-		len = sqrt(len);
-		if(len == 0) len = 0.00001;
-		for(int j = 1; j <= frameWidth; j++) { // 归一化 for test
-			tmp(i, j) /= len;
 		}
 	}
 
@@ -101,10 +107,17 @@ void readSingleData(string dataPathStr, int idx, vector<Matrix<double>*> &data, 
 	// 通过滑动窗口来得到训练数据
 	for(int i = 1; i <= frameNum - windowWidth + 1; i++) {
 		Matrix<double> *win = new Matrix<double>(windowWidth, frameWidth);
+		double len = 0; // 模长
 		for(int row = 1; row <= windowWidth; row++)
 			for(int col = 1; col <= frameWidth; col++) {
 				(*win)(row, col) = tmp(i+row-1, col);
+				len += (*win)(row, col) * (*win)(row, col);
 			}
+		len = sqrt(len);
+		if(len == 0) len = 0.00001;
+		len /= 100; // 将模长设置为100, 增大刺激的效果
+		FOR(x, 1, windowWidth) FOR(y, 1, frameWidth) (*win)(x, y) /= len; // 单位化
+
 		data.push_back(win);
 		label.push_back(lab);
 	}
@@ -152,21 +165,21 @@ void train() {
 	FuncAbstractor functionAbstractor(Input, Output, estimator, 0.1);
 
 	Optimizer optimizer(
-		&functionAbstractor,
-		0.05,
-		10000,
-		trainData,
-		trainLabel,
-		"train_backup",
-		2333,
-		-0.20, 0.20, 0.000001,
-		100
-	);
+			&functionAbstractor,
+			0.0001,
+			20000,
+			trainData,
+			trainLabel,
+			"train_backup",
+			2333,
+			-0.20, 0.20, 0.000001,
+			100
+			);
 
 	// reg the dropout layers
-	//optimizer.AddDropoutLayer(Dp1);
-	//optimizer.AddDropoutLayer(Dp2);
-	//optimizer.AddDropoutLayer(Dp3);
+	optimizer.AddDropoutLayer(Dp1);
+	optimizer.AddDropoutLayer(Dp2);
+	optimizer.AddDropoutLayer(Dp3);
 
 	optimizer.SetSaveStep(5);
 	optimizer.TrainFromFile();
@@ -180,21 +193,21 @@ void test() {
 	FuncAbstractor functionAbstractor(Input, Output, estimator, 0.1);
 
 	Predictor predictor(
-		&functionAbstractor,
-		0.1,
-		2000,
-		trainData,
-		trainLabel,
-		"train_backup",
-		2333,
-		-0.20, 0.20, 0.000001,
-		100
-	);
+			&functionAbstractor,
+			0.05,
+			2000,
+			trainData,
+			trainLabel,
+			"train_backup",
+			2333,
+			-0.20, 0.20, 0.000001,
+			100
+			);
 
 	// reg the dropout layers
-	//predictor.AddDropoutLayer(Dp1);
-	//predictor.AddDropoutLayer(Dp2);
-	//predictor.AddDropoutLayer(Dp3);
+	predictor.AddDropoutLayer(Dp1);
+	predictor.AddDropoutLayer(Dp2);
+	predictor.AddDropoutLayer(Dp3);
 
 	Matrix<double> *mat;
 	FOR(i, 1, wordList.size()) {
@@ -219,7 +232,9 @@ void test() {
 
 int main() {
 	//GenMfccFiles();
+#ifdef ENABLE_CUDA
 	cuda_init();
+#endif
 	buildNetwork();
 	readTrainData("../data", 0.8);
 	//train();
